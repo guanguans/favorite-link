5018b90b01eb8c84fdc6c517d469be5b820c3d28
guanguans <ityaozm@gmail.com>
2025-02-05 14:11:24 +0800

    ci(rector): Refactor rector configuration for improved clarity
    - Clean up and enhance the rector configuration setup
    - Remove commented-out code and unnecessary imports
    - Streamline path specifications and configurations for better maintainability
    - Adjust rules and presets to align with PHP 8.4 features
    - Ensure alternative processing methods for skipping files are clarified

--- rector.php
+++ rector.php
@@ -1,5 +1,8 @@
 <?php
 
+/** @noinspection PhpUnhandledExceptionInspection */
+/** @noinspection PhpInternalEntityUsedInspection */
+
 declare(strict_types=1);
 
 /**
@@ -11,7 +14,6 @@ declare(strict_types=1);
  * @see https://github.com/guanguans/favorite-link
  */
 
-use Rector\Caching\ValueObject\Storage\FileCacheStorage;
 use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
 use Rector\CodeQuality\Rector\LogicalAnd\LogicalToBooleanRector;
 use Rector\CodingStyle\Rector\ArrowFunction\StaticArrowFunctionRector;
@@ -28,56 +30,99 @@ use Rector\Naming\Rector\Foreach_\RenameForeachValueVariableToMatchMethodCallRet
 use Rector\Php73\Rector\String_\SensitiveHereNowDocRector;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\AddSeeTestAnnotationRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
-use Rector\Removing\Rector\Class_\RemoveTraitUseRector;
 use Rector\Renaming\Rector\FuncCall\RenameFunctionRector;
-use Rector\Set\ValueObject\DowngradeLevelSetList;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 use Rector\Transform\Rector\ClassMethod\ReturnTypeWillChangeRector;
 use Rector\Transform\Rector\FileWithoutNamespace\RectorConfigBuilderRector;
-use Rector\Transform\Rector\String_\StringToClassConstantRector;
 use Rector\Transform\ValueObject\ClassMethodReference;
-use Rector\Transform\ValueObject\StringToClassConstant;
-use Rector\ValueObject\PhpVersion;
 
-return static function (RectorConfig $rectorConfig): void {
-    // \define('MHASH_XXH3', 2 << 0);
-    // \define('MHASH_XXH32', 2 << 1);
-    // \define('MHASH_XXH64', 2 << 2);
-    // \define('MHASH_XXH128', 2 << 3);
-    $rectorConfig->importNames(false, false);
-    $rectorConfig->importShortClasses(false);
-    $rectorConfig->parallel(240);
-    // $rectorConfig->disableParallel();
-    // $rectorConfig->phpstanConfig(__DIR__.'/phpstan.neon');
-    $rectorConfig->phpVersion(PhpVersion::PHP_84);
-    // $rectorConfig->cacheClass(FileCacheStorage::class);
-    // $rectorConfig->cacheDirectory(__DIR__.'/.build/rector');
-    // $rectorConfig->containerCacheDirectory(__DIR__.'/.build/rector');
-    // $rectorConfig->disableParallel();
-    // $rectorConfig->fileExtensions(['php']);
-    // $rectorConfig->indent(' ', 4);
-    // $rectorConfig->memoryLimit('2G');
-    // $rectorConfig->noDiffs();
-    // $rectorConfig->removeUnusedImports();
-
-    $rectorConfig->bootstrapFiles([
-        // __DIR__.'/vendor/autoload.php',
-    ]);
-
-    $rectorConfig->autoloadPaths([
-        // __DIR__.'/vendor/autoload.php',
-    ]);
-
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__.'/app',
         __DIR__.'/tests',
         __DIR__.'/.*.php',
         __DIR__.'/*.php',
         __DIR__.'/composer-updater',
-    ]);
+    ])
+    ->withCache(__DIR__.'/.build/rector/')
+    ->withParallel()
+    // ->withoutParallel()
+    ->withImportNames(false)
+    ->withAttributesSets(
+        symfony : true,
+        doctrine : true,
+        mongoDb : true,
+        gedmo : true,
+        phpunit : true,
+        fosRest : true,
+        jms : true,
+        sensiolabs : true,
+        behat : true,
+        all : true
+    )
+    // ->withDeadCodeLevel(42)
+    // ->withTypeCoverageLevel(37)
+    ->withFluentCallNewLine()
+    // ->withPhp74Sets()
+    ->withPhpSets(php84: true)
+    // ->withDowngradeSets(php84: true)
+    ->withPreparedSets(
+        deadCode : \true,
+        codeQuality : \true,
+        codingStyle : \true,
+        typeDeclarations : true,
+        privatization : true,
+        naming : true,
+        instanceOf : true,
+        earlyReturn : true,
+        // strictBooleans : true,
+        carbon : true,
+        rectorPreset : true,
+        phpunitCodeQuality : true,
+        doctrineCodeQuality : true,
+        symfonyCodeQuality : true,
+        symfonyConfigs : true
+    )
+    ->withSets([
+        PHPUnitSetList::PHPUNIT_100,
+        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
+        PHPUnitSetList::ANNOTATIONS_TO_ATTRIBUTES,
+    ])
+    ->withComposerBased(twig: true, doctrine: true, phpunit: true, symfony: true)
+    ->withRules([
+        // SortAssociativeArrayByKeyRector::class,
+        AddSeeTestAnnotationRector::class,
+        DowngradeArrayFilterNullableCallbackRector::class,
+        RectorConfigBuilderRector::class,
+        StaticArrowFunctionRector::class,
+        StaticClosureRector::class,
+    ])
+    ->withConfiguredRule(
+        RenameFunctionRector::class,
+        [
+            'test' => 'it',
+        ] + array_reduce(
+            [
+                // 'make',
+                // 'env_explode',
+            ],
+            static function (array $carry, string $func): array {
+                /** @see https://github.com/laravel/framework/blob/11.x/src/Illuminate/Support/functions.php */
+                $carry[$func] = "Guanguans\\SoarPHP\\Support\\$func";
 
-    $rectorConfig->skip([
+                return $carry;
+            },
+            []
+        )
+    )
+    ->withConfiguredRule(ReturnTypeWillChangeRector::class, [
+        new ClassMethodReference(ArrayAccess::class, 'offsetGet'),
+    ])
+    ->withSkip([
+        '**/__snapshots__/*',
+        '**/Fixtures/*',
+        __DIR__.'/tests.php',
+    ])
+    ->withSkip([
         AddSeeTestAnnotationRector::class,
         DowngradeArrayFilterNullableCallbackRector::class,
         EncapsedStringsToSprintfRector::class,
@@ -88,123 +133,16 @@ return static function (RectorConfig $rectorConfig): void {
         ReturnBinaryOrToEarlyReturnRector::class,
         SensitiveHereNowDocRector::class,
         WrapEncapsedVariableInCurlyBracesRector::class,
+    ])
+    ->withSkip([
         RemoveEmptyClassMethodRector::class => [
             __DIR__.'/app/Providers/AppServiceProvider.php',
         ],
         RenameForeachValueVariableToMatchMethodCallReturnTypeRector::class => [
             __DIR__.'/tests/Pest.php',
         ],
-        StaticClosureRector::class => [
+        StaticArrowFunctionRector::class => $staticClosureSkipPaths = [
             __DIR__.'/tests',
         ],
-
-        // paths
-        __DIR__.'/tests.php',
-        '**/Fixture*',
-        '**/Fixture/*',
-        '**/Fixtures*',
-        '**/Fixtures/*',
-        '**/Stub*',
-        '**/Stub/*',
-        '**/Stubs*',
-        '**/Stubs/*',
-        '**/Source*',
-        '**/Source/*',
-        '**/Expected/*',
-        '**/Expected*',
-        '**/__snapshots__/*',
-        '**/__snapshots__*',
+        StaticClosureRector::class => $staticClosureSkipPaths,
     ]);
-
-    $rectorConfig->sets([
-        // DowngradeLevelSetList::DOWN_TO_PHP_84,
-        LevelSetList::UP_TO_PHP_84,
-        SetList::CODE_QUALITY,
-        SetList::CODING_STYLE,
-        SetList::DEAD_CODE,
-        // SetList::STRICT_BOOLEANS,
-        // SetList::GMAGICK_TO_IMAGICK,
-        SetList::NAMING,
-        SetList::PRIVATIZATION,
-        SetList::TYPE_DECLARATION,
-        SetList::EARLY_RETURN,
-        SetList::INSTANCEOF,
-
-        PHPUnitSetList::PHPUNIT_100,
-        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
-        PHPUnitSetList::ANNOTATIONS_TO_ATTRIBUTES,
-    ]);
-
-    $rectorConfig->rules([
-        // SortAssociativeArrayByKeyRector::class,
-        // ToInternalExceptionRector::class,
-        AddSeeTestAnnotationRector::class,
-        DowngradeArrayFilterNullableCallbackRector::class,
-        StaticArrowFunctionRector::class,
-        StaticClosureRector::class,
-        RectorConfigBuilderRector::class,
-    ]);
-
-    $rectorConfig->ruleWithConfiguration(RenameFunctionRector::class, [
-        'test' => 'it',
-    ]);
-
-    $rectorConfig->ruleWithConfiguration(ReturnTypeWillChangeRector::class, [
-        new ClassMethodReference(ArrayAccess::class, 'offsetGet'),
-    ]);
-
-    // (function ($rectorConfig): void {
-    //     $rectorConfig->ruleConfigurations[AnnotationToAttributeRector::class] = array_filter(
-    //         $rectorConfig->ruleConfigurations[AnnotationToAttributeRector::class],
-    //         static fn (AnnotationToAttribute $annotationToAttribute): bool => ! in_array(
-    //             $annotationToAttribute->getAttributeClass(),
-    //             [
-    //                 CodeCoverageIgnore::class,
-    //             ],
-    //             true
-    //         )
-    //     );
-    // })->call($rectorConfig, $rectorConfig);
-
-    // $rectorConfig->ruleWithConfiguration(RenameFunctionRector::class, [
-    //     'encrypt' => 'encrypt_old',
-    //     'decrypt' => 'decrypt_old',
-    // ]);
-    //
-    // $rectorConfig->ruleWithConfiguration(RenameAnnotationRector::class, [
-    //     new RenameAnnotationByType('PHPUnit\Framework\TestCase', 'test', 'scenario'),
-    //     new RenameAnnotation('', 'unknown'),
-    // ]);
-    //
-    // $rectorConfig->ruleWithConfiguration(RenameMethodRector::class, [
-    //     new MethodCallRename('SomeExampleClass', 'oldMethod', 'newMethod'),
-    // ]);
-    //
-    // $rectorConfig->ruleWithConfiguration(RenameStaticMethodRector::class, [
-    //     new RenameStaticMethod('SomeClass', 'oldMethod', 'AnotherExampleClass', 'newStaticMethod'),
-    // ]);
-    //
-    // $rectorConfig->ruleWithConfiguration(RenamePropertyRector::class, [
-    //     new RenameProperty('SomeClass', 'someOldProperty', 'someNewProperty'),
-    // ]);
-    //
-    // $rectorConfig->ruleWithConfiguration(RenameStringRector::class, [
-    //     'ROLE_PREVIOUS_ADMIN' => 'IS_IMPERSONATOR',
-    // ]);
-    //
-    // $rectorConfig->ruleWithConfiguration(RemoveFuncCallRector::class, [
-    //     'var_dump',
-    // ]);
-    //
-    // $rectorConfig->ruleWithConfiguration(RemoveInterfacesRector::class, [
-    //     'SomeInterface',
-    // ]);
-    //
-    // $rectorConfig->ruleWithConfiguration(RemoveTraitUseRector::class, [
-    //     'TraitNameToRemove',
-    // ]);
-    //
-    // $rectorConfig->ruleWithConfiguration(StringToClassConstantRector::class, [
-    //     new StringToClassConstant('compiler.post_dump', 'Yet\AnotherClass', 'CONSTANT'),
-    // ]);
-};
