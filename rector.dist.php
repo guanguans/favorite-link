9689229a497e423931bc1f3f6d03accc29f75e1b
guanguans <ityaozm@gmail.com>
2026-08-12 23:30:34 +0800

    deps: update illuminate/validation and illuminate/database versions
    - Upgrade illuminate/validation to ^13.25 for compatibility with newer features.
    - Upgrade illuminate/database to ^13.25 to ensure consistency across dependencies.
    - Update pestphp/pest to ^5.1 and pestphp/pest-plugin-rector to ^5.0 for improved testing capabilities.
    
    Signed-off-by: guanguans <ityaozm@gmail.com>

--- rector.dist.php
+++ rector.dist.php
@@ -19,6 +19,7 @@ use Ergebnis\Rector\Rules\Faker\GeneratorPropertyFetchToMethodCallRector;
 use Guanguans\RectorRules\Rector\File\AddNoinspectionDocblockToFileFirstStmtRector;
 use Guanguans\RectorRules\Rector\Name\RenameToConventionalCaseNameRector;
 use Guanguans\RectorRules\Set\SetList;
+use Pest\Rector\Set\PestSetList;
 use PhpParser\NodeVisitor\ParentConnectingVisitor;
 use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
 use Rector\CodeQuality\Rector\LogicalAnd\LogicalToBooleanRector;
@@ -30,13 +31,16 @@ use Rector\CodingStyle\Rector\Encapsed\WrapEncapsedVariableInCurlyBracesRector;
 use Rector\CodingStyle\Rector\Enum_\EnumCaseToPascalCaseRector;
 use Rector\CodingStyle\Rector\FuncCall\ArraySpreadInsteadOfArrayMergeRector;
 use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\ClassLike\RemoveAnnotationRector;
 use Rector\DeadCode\Rector\ClassMethod\RemoveEmptyClassMethodRector;
 use Rector\EarlyReturn\Rector\If_\ChangeOrIfContinueToMultiContinueRector;
 use Rector\EarlyReturn\Rector\Return_\ReturnBinaryOrToEarlyReturnRector;
 use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
 use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
+use Rector\Php82\Rector\Param\AddSensitiveParameterAttributeRector;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitThisCallRector;
 use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
+use Rector\Transform\Rector\Scalar\ScalarValueToConstFetchRector;
 use Rector\Transform\Rector\String_\StringToClassConstantRector;
 use Rector\ValueObject\PhpVersion;
 use RectorLaravel\Rector\ArrayDimFetch\ArrayToArrGetRector;
@@ -48,8 +52,6 @@ use RectorLaravel\Rector\If_\ThrowIfRector;
 use RectorLaravel\Rector\MethodCall\ContainerBindConcreteWithClosureOnlyRector;
 use RectorLaravel\Rector\StaticCall\DispatchToHelperFunctionsRector;
 use RectorLaravel\Set\LaravelSetProvider;
-use RectorPest\Set\PestLevelSetList;
-use RectorPest\Set\PestSetList;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -97,17 +99,13 @@ return RectorConfig::configure()
     )
     ->withSets([
         SetList::ALL,
-        PestLevelSetList::UP_TO_PEST_40,
-        PestSetList::PEST_CHAIN,
-        PestSetList::PEST_CODE_QUALITY,
-        PestSetList::PEST_LARAVEL,
-        PestSetList::PEST_MIGRATION,
+        PestSetList::CODING_STYLE,
     ])
     ->withRules([
-        ArraySpreadInsteadOfArrayMergeRector::class,
+        // ArraySpreadInsteadOfArrayMergeRector::class,
         // EnumCaseToPascalCaseRector::class,
         GeneratorPropertyFetchToMethodCallRector::class,
-        JsonThrowOnErrorRector::class,
+        // JsonThrowOnErrorRector::class,
         SortAssociativeArrayByKeyRector::class,
         // StaticArrowFunctionRector::class,
         // StaticClosureRector::class,
@@ -126,15 +124,19 @@ return RectorConfig::configure()
     ->registerDecoratingNodeVisitor(ParentConnectingVisitor::class)
     ->withConfiguredRule(RenameToConventionalCaseNameRector::class, ['afterEach', 'beforeEach', 'MIT', 'PDO'])
     ->withSkip([
-        ChangeOrIfContinueToMultiContinueRector::class,
-        DisallowedEmptyRuleFixerRector::class,
-        EncapsedStringsToSprintfRector::class,
-        ExplicitBoolCompareRector::class,
+        AddSensitiveParameterAttributeRector::class,
+        RemoveAnnotationRector::class,
+        ScalarValueToConstFetchRector::class,
+
+        // ChangeOrIfContinueToMultiContinueRector::class,
+        // DisallowedEmptyRuleFixerRector::class,
+        // EncapsedStringsToSprintfRector::class,
+        // ExplicitBoolCompareRector::class,
         LogicalToBooleanRector::class,
         NewlineBetweenClassLikeStmtsRector::class,
         PreferPHPUnitThisCallRector::class,
-        ReturnBinaryOrToEarlyReturnRector::class,
-        WrapEncapsedVariableInCurlyBracesRector::class,
+        // ReturnBinaryOrToEarlyReturnRector::class,
+        // WrapEncapsedVariableInCurlyBracesRector::class,
     ])
     ->withSkip([
         ContainerBindConcreteWithClosureOnlyRector::class,
@@ -148,9 +150,9 @@ return RectorConfig::configure()
         ThrowIfRector::class,
     ])
     ->withSkip([
-        JsonThrowOnErrorRector::class => [
-            __DIR__.'/tests/Pest.php',
-        ],
+        // JsonThrowOnErrorRector::class => [
+        //     __DIR__.'/tests/Pest.php',
+        // ],
         RemoveEmptyClassMethodRector::class => [
             __DIR__.'/app/Providers/AppServiceProvider.php',
         ],
